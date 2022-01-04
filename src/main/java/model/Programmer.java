d3ae4c8d14d6fe37c3bdc6ca1f990295f9220351
neda <nedaakbari555@gmail.com>
2022-01-04 11:58:53 +0330

    Programmer class with enum Gender added

---
+++ src/main/java/model/Programmer.java
@@ -0,0 +1,27 @@
+package model;
+
+import model.enums.Gender;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Programmer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String lastName;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    public Programmer(String firstName, String lastName, Gender gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+    }
+}
